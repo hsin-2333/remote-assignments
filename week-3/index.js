0572c97841d3b27531b413b94f5b65485be9f3f6
hsinyi <yuanqi1368@gmail.com>
2024-07-02 14:38:23 +0800

    assignment3: Connect to Backend API by AJAX

--- week-3/index.js
+++ week-3/index.js
@@ -6,6 +6,28 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/getData', (req, res) => {
+    if (!req.query.number) {
+        res.send("Lack of Parameter");
+    }
+    else{
+        const number = Number(req.query.number,10);
+        // const number = parseInt(req.query.number,10);
+        console.log(number);
+        if (isNaN(number)){
+            res.send('Wrong Parameter');
+        }
+        else{
+            let result = (1+number)*number/2;
+            res.send(`${result}`);
+        }
+    }
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.use(express.static('public'));
