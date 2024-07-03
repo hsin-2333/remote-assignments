c7590df7e0e7eb9ccfa79587c5d7dff37514e178
hsinyi <yuanqi1368@gmail.com>
2024-07-03 16:18:48 +0800

    Assignment5: HTTP Cookie

--- week-3/index.js
+++ week-3/index.js
@@ -1,11 +1,41 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const cookieParser = require('cookie-parser'); 
+const { use } = require('react');
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//Check cookies for the user's name in the backend
+app.get('/myName',(req, res)=>{
+    if (Object.keys(req.cookies).length === 0){
+        // res.send(Object.keys(req.cookies));
+        res.send('No cookie')
+    }
+    else{
+        res.send(Object.keys(req.cookies));
+        console.log('有cookie',Object.keys(req.cookies));        
+    }
+    // console.log('test',req.cookies);
+    // res.send('Check your console'+`${Object.keys(userCookie)}`);
+})
+
+//Save the user's input in the cookie
+app.get('/trackName',(req, res)=>{
+    //將input存入cookie
+    if (req.query.name){ 
+        res.cookie(req.query.name);
+        // res.send('Yeah🎉🎉 Welcome Here, '+ `${Object.keys(req.cookies)}`);
+        res.redirect('/myName.html');
+    }
+    else{
+        res.send('Lack of Parameter');
+    }
+})
 
 app.get('/getData', (req, res) => {
     if (!req.query.number) {
