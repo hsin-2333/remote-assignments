05e9902c1b3f933812dc4375d99a0fc0eacf1a83
hsinyi <yuanqi1368@gmail.com>
2024-07-11 13:19:09 +0800

    gh-page: vite root revise

--- week-4/vite.config.js
+++ week-4/vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        assignment3: 'assignment3/index3.html',
+        assignment4: 'assignment4/index4.html',
+        assignment5: 'assignment5/index5.html',
+      }
+    }
+  },
   plugins: [react()],
   base: '/remote-assignments/week-4/'
 })
