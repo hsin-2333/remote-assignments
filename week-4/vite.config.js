d676b7fbb009d040f6c5276c6a614a017fe57afa
hsinyi <yuanqi1368@gmail.com>
2024-07-11 11:07:40 +0800

    gitpage setting

--- week-4/vite.config.js
+++ week-4/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/remote-assignments/week-4/' // 確保這裡的 repository-name 是你的 GitHub 倉庫名稱
 })
