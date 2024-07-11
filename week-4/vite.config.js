a79f81165fbd3266e815d9672fe7722a27b669ad
hsinyi <yuanqi1368@gmail.com>
2024-07-11 14:40:34 +0800

    update path

--- week-4/vite.config.js
+++ week-4/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; 
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        assignment3: 'assignment3/index3.html',
-        assignment4: 'assignment4/index4.html',
-        assignment5: 'assignment5/index5.html',
+        main: path.resolve(__dirname, "index.html"),
+        assignment3: path.resolve(__dirname, "assignment3/index3.html"),
+        assignment4: path.resolve(__dirname, "assignment4/index4.html"),
+        assignment5: path.resolve(__dirname, "assignment5/index5.html"),
       }
     }
   },
